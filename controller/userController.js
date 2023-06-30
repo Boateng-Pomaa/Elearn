94a0c035271d2d154cf67c8f50f65a5cb0dc194b
Boateng Pomaa Linda <lindapomaa27@gmail.com>
2023-06-30 19:46:44 +0100

    added gitignore

--- controller/userController.js
+++ controller/userController.js
@@ -1,67 +1,71 @@
-import {userModel} from '../Model/userSchema.js'
+import { userModel } from '../Model/userSchema.js'
 import * as bcrypt from "bcrypt"
 
 
-export async function loginUser(req,res){
+export async function loginUser(req, res) {
     try {
-        const {username, password} = req.body
-        const user = await userModel.findOne({username:username})
-        if(user &&(await bcrypt.compare(password, user.password)))
-           {
-             res.status(200).json({
-                    message:"Login successful",
-                    user
-                })
-            }else{
-                res.status(400).json({
-                    message:'Invalid Credentials'
-                })
+        const { username, password } = req.body
+        const user = await userModel.findOne({ username: username })
+        if (user && (await bcrypt.compare(password, user.password))) {
+            res.status(200).json({
+                message: "Login successful",
+                user
+            })
+        } else {
+            res.status(400).json({
+                message: 'Invalid Credentials'
+            })
 
-            }
         }
-     catch (err) {
+    }
+    catch (err) {
         console.error(err.message)
         res.status(500).json({
-            message:'server error'
+            message: 'server error'
         })
     }
 }
 
 
 
-export async function registerUser(req,res){
-    const {username, password} = req.body;
+export async function registerUser(req, res) {
+    try {
+        const { username, password } = req.body;
 
-    // Validation
-  if (!username || !password ) {
-    res.status(400).json({
-        message:'Please include all fields'})
-  }
+        // Validation
+        if (!username || !password) {
+            return res.status(400).json({
+                message: 'Please include all fields'
+            })
+        }
 
-  // Find if user already exists
-  const userExists = await userModel.findOne({ username })
+        // Find if user already exists
+        const userExists = await userModel.findOne({ username })
 
-  if (userExists) {
-    res.status(400).json({
-        message:'Account already exists'})
-  }
+        if (userExists) {
+            return res.status(400).json({
+                message: 'Account already exists'
+            })
+        }
 
-  // CREATING USER
-    const user = await userModel.create({
-        username,
-        password
-    })
-    if (user){
-        res.status(200).json({
-            message:'Registration Successful',
-            user,
+        // CREATING USER
+        const user = await userModel.create({
+            username,
+            password
         })
-    }else{
-        res.status(400).json({
-            message:"Registration unsuccessful"
-        })
+        if (user) {
+            res.status(200).json({
+                message: 'Registration Successful',
+                user,
+            })
+        } else {
+            res.status(400).json({
+                message: "Registration unsuccessful"
+            })
+        }
+    }
+    catch (error) {
+        console.log(error)
+        res.status(500).send('Internal Server Error')
     }
 }
-
-
-
