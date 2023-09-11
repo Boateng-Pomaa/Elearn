34a1b4132af6368b0d7a25419eaffc4b1a00eaa2
Boateng Pomaa Linda <lindapomaa27@gmail.com>
2023-09-12 00:42:06 +0100

    nvm

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,14 +2,14 @@ import express from 'express'
 const router = express.Router()
 import { check } from 'express-validator'
 import { registerUser, loginUser, profile, feed, post, search, addAnswer, yourQuestion, yourAnswer } from '../controller/userController.js'
-import {upvoteQuestion,downVoteQuestion,upvoteAnswer,downVoteAnswer,specificQuestion, viewAnswers} from '../controller/userController.js'
+import { upvoteQuestion, downVoteQuestion, upvoteAnswer, downVoteAnswer, specificQuestion, viewAnswers } from '../controller/userController.js'
 
 
 
 router.post('/signup', registerUser)
-router.post('/login', [
-  check("A valid password is required").isLength({ min: 4 })
-], loginUser)
+  .post('/login', [
+    check("A valid password is required").isLength({ min: 4 })
+  ], loginUser)
   .get('/search', search)
   .get('/feed', feed)
   .get('/profile/:id', profile)
@@ -17,11 +17,11 @@ router.post('/login', [
   .post('/answer/:id', addAnswer)
   .get('/userQuestions/:id', yourQuestion)
   .get('/userAnswers/:id', yourAnswer)
-  .post('/upvoteQ/:questionId',upvoteQuestion)
-  .post('/downvoteQ/:questionId',downVoteQuestion)
-  .post('/upvoteA/:answerId',upvoteAnswer)
-  .post('/downvoteA/:answerId',downVoteAnswer)
-  .get('/viewQuestion',specificQuestion)
-  .get('/viewAnswers/:questionId',viewAnswers)
+  .post('/upvoteQ/:questionId', upvoteQuestion)
+  .post('/downvoteQ/:questionId', downVoteQuestion)
+  .post('/upvoteA/:answerId', upvoteAnswer)
+  .post('/downvoteA/:answerId', downVoteAnswer)
+  .get('/viewQuestion', specificQuestion)
+  .get('/viewAnswers/:questionId', viewAnswers)
 
 export default router
